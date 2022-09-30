66937c1da2e05c73a554d046517fc04111961ebb
MRezaFtr <rezaakku@gmail.com>
2022-09-30 22:26:43 +0700

    Tugas Pertemuan 3

---
+++ application/views/v_about.php
@@ -0,0 +1,24 @@
+<section>
+    <h1><?php   echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Muhammad Reza</li>
+        <li>Nama Belakang : Fitriansyah</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Re. Martadinata , Gg.Alpokat</li>
+    </ul>
+
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Makassar</li>
+    </ul>
+
+    <h4>Olahraga Favorit</h4>
+    <ul type="square">
+        <li>Silat</li>
+        <li>Bulutangkis</li>
+    </ul>
+</section>
